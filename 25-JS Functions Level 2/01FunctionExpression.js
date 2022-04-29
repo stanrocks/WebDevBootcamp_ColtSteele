c3cac0f696501c3a1cd0013e6a2db94bc340c720
stanrocks <25880632+stanrocks@users.noreply.github.com>
2022-04-30 04:03:18 +0500

    JS Functions Level 2 Complete

---
+++ 25-JS Functions Level 2/01FunctionExpression.js
@@ -0,0 +1,8 @@
+// Define a function that returns the square of a number. Define the function as a function expression, stored in a variable called square.  NOTE: Udemy's coding exercise platform does NOT support the ** operator.
+
+// square(4) //16
+// square(3) //9
+
+const square = function (num) {
+  return num ** 2;
+};
